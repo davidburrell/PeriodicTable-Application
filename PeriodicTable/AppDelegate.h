7eaae5baabc931c1f7c3768f0679688658cf0e26
David Burrell <davidburrell@fullsail.edu>
2015-01-08 22:38:53 -0500

    Initial Commit

---
+++ PeriodicTable/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PeriodicTable
+//
+//  Created by david burrell on 1/8/15.
+//  Copyright (c) 2015 SimpleGenius. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ PeriodicTable/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  PeriodicTable
+//
+//  Created by david burrell on 1/8/15.
+//  Copyright (c) 2015 SimpleGenius. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
